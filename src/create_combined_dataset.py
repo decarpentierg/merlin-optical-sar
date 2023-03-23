7228c2a7a4a92ca3796d8a2251f450f607a23b6c
Gonzague de Carpentier <gonzague.de-carpentier@polytechnique.edu>
2023-03-23 11:12:33 +0100

    reorganize project and blacken code

--- src/create_combined_dataset.py
+++ src/create_combined_dataset.py
@@ -1,118 +1,153 @@
-# Navigate to appropriate directory
 import os
-while os.path.split(os.getcwd())[1] != "merlin-optical-sar":
-    os.chdir("..")
-    if os.getcwd() == "/":
-        raise ValueError()
-print("Current working directory: {}".format(os.getcwd()))
 import numpy as np
 import argparse
-
-import sys
-sys.path.insert(0, './src')
 import mvalab
-import utils
+import vistools
 
-parser = argparse.ArgumentParser(description='Extraction des données pour MERLIN')
-parser.add_argument('--dataset', dest='dataset', default='Saclay', help='dataset to use : Saclay or Sendai')
-parser.add_argument('--method', dest='method', default='SAR', help='method to use : SAR, SAR+OPT, SAR+SAR or SAR+OPT+SAR')
+parser = argparse.ArgumentParser(description="Extraction des données pour MERLIN")
+parser.add_argument(
+    "--dataset", dest="dataset", default="Saclay", help="dataset to use : Saclay or Sendai"
+)
+parser.add_argument(
+    "--method",
+    dest="method",
+    default="SAR",
+    help="method to use : SAR, SAR+OPT, SAR+SAR or SAR+OPT+SAR",
+)
 args = parser.parse_args()
 
-if args.dataset != 'Saclay' and args.dataset != 'Sendai':
-    raise ValueError('Dataset must be either Saclay or Sendai')
-if args.method != 'SAR' and args.method != 'SAR+OPT' and args.method != 'SAR+SAR' and args.method != 'SAR+OPT+SAR':
-    raise ValueError('Method must be either SAR, SAR+OPT, SAR+SAR or SAR+OPT+SAR')
+if args.dataset != "Saclay" and args.dataset != "Sendai":
+    raise ValueError("Dataset must be either Saclay or Sendai")
+if (
+    args.method != "SAR"
+    and args.method != "SAR+OPT"
+    and args.method != "SAR+SAR"
+    and args.method != "SAR+OPT+SAR"
+):
+    raise ValueError("Method must be either SAR, SAR+OPT, SAR+SAR or SAR+OPT+SAR")
 
-#import
-if args.dataset == 'Saclay':
-    sar_no_register = mvalab.imz2mat('data/Saclay/TelecomParisZ4.IMA')[0]
-    opt_no_register = mvalab.imz2mat('data/Saclay/TelecomParisOPT.IMA')[0][:, :, 0] #only the first image is relevant
+# import
+if args.dataset == "Saclay":
+    sar_no_register = mvalab.imz2mat("data/Saclay/TelecomParisZ4.IMA")[0]
+    opt_no_register = mvalab.imz2mat("data/Saclay/TelecomParisOPT.IMA")[0][
+        :, :, 0
+    ]  # only the first image is relevant
     di, dj = -19, 147  # define registering translation
 else:
-    opt_no_register = mvalab.imz2mat('data/Sendai/PileTSX_AVANT_surTSX_1024x1024RECALOPT.IMA')[0][:988, 33:]
-    sar_no_register = mvalab.imz2mat('data/Sendai/PileTSX_AVANT_surTSX_1024x1024RECALZ4.IMA')[0][:988, 33:,:]
+    opt_no_register = mvalab.imz2mat("data/Sendai/PileTSX_AVANT_surTSX_1024x1024RECALOPT.IMA")[
+        0
+    ][:988, 33:]
+    sar_no_register = mvalab.imz2mat("data/Sendai/PileTSX_AVANT_surTSX_1024x1024RECALZ4.IMA")[
+        0
+    ][:988, 33:, :]
     di, dj = 5, -23  # define registering translation
 
 # register images
 opt0 = np.copy(opt_no_register)
-l,L = utils.register(opt0, sar_no_register[:, :, 0], di, dj)[0].shape
-sar = np.zeros((l,L,sar_no_register.shape[-1]), dtype=complex)
+l, L = vistools.register(opt0, sar_no_register[:, :, 0], di, dj)[0].shape
+sar = np.zeros((l, L, sar_no_register.shape[-1]), dtype=complex)
 for i in range(sar_no_register.shape[-1]):
-    opt, sar[:, :, i]  = utils.register(opt0, sar_no_register[:, :, i],  di, dj)
+    opt, sar[:, :, i] = vistools.register(opt0, sar_no_register[:, :, i], di, dj)
 
 # check if folder exists
-folder_name = 'dataset_'+args.dataset+'_'+args.method
+folder_name = "dataset_" + args.dataset + "_" + args.method
 if not os.path.exists(folder_name):
     os.mkdir(folder_name)
-    os.mkdir(folder_name+'/train')
-    os.mkdir(folder_name+'/validation')
-    os.mkdir(folder_name+'/train/spotlight')
-    os.mkdir(folder_name+'/validation/spotlight')
+    os.mkdir(folder_name + "/train")
+    os.mkdir(folder_name + "/validation")
+    os.mkdir(folder_name + "/train/spotlight")
+    os.mkdir(folder_name + "/validation/spotlight")
 
-if args.method == 'SAR':
+if args.method == "SAR":
     nb_dim_sup = 0
-elif args.method == 'SAR+OPT':
+elif args.method == "SAR+OPT":
     nb_dim_sup = 1
-elif args.method == 'SAR+SAR':
+elif args.method == "SAR+SAR":
     nb_dim_sup = 1
-elif args.method == 'SAR+OPT+SAR':
+elif args.method == "SAR+OPT+SAR":
     nb_dim_sup = 2
 
-#training data
+# training data
 for i in range(sar.shape[-1]):
-    img_train_combined = np.zeros((l,L,2+nb_dim_sup))
-    img_train_combined[:,:,0] = np.real(sar[:,:,i])
-    img_train_combined[:,:,1] = np.imag(sar[:,:,i])
-    if args.method == 'SAR+OPT':
-        img_train_combined[:,:,2] = opt
-    elif args.method == 'SAR+SAR':
-        if args.dataset == 'Saclay':
-            img_train_combined[:,:,2] = np.abs(sar[:,:,3]) #higher resolution SAR image
-            if i >=2: # in this case we only use the first three sar images
+    img_train_combined = np.zeros((l, L, 2 + nb_dim_sup))
+    img_train_combined[:, :, 0] = np.real(sar[:, :, i])
+    img_train_combined[:, :, 1] = np.imag(sar[:, :, i])
+    if args.method == "SAR+OPT":
+        img_train_combined[:, :, 2] = opt
+    elif args.method == "SAR+SAR":
+        if args.dataset == "Saclay":
+            img_train_combined[:, :, 2] = np.abs(sar[:, :, 3])  # higher resolution SAR image
+            if i >= 2:  # in this case we only use the first three sar images
                 break
         else:
-            img_train_combined[:,:,2] = np.abs(sar[:,:,1])
-            break # only two sar images in Sendai
-    elif args.method == 'SAR+OPT+SAR':
-        img_train_combined[:,:,2] = opt
-        if args.dataset == 'Saclay':
-            img_train_combined[:,:,3] = np.abs(sar[:,:,3])
-            if i >=2:
+            img_train_combined[:, :, 2] = np.abs(sar[:, :, 1])
+            break  # only two sar images in Sendai
+    elif args.method == "SAR+OPT+SAR":
+        img_train_combined[:, :, 2] = opt
+        if args.dataset == "Saclay":
+            img_train_combined[:, :, 3] = np.abs(sar[:, :, 3])
+            if i >= 2:
                 break
         else:
-            img_train_combined[:,:,3] = np.abs(sar[:,:,1])
+            img_train_combined[:, :, 3] = np.abs(sar[:, :, 1])
             break
-    np.save(folder_name+'/train/spotlight/train_data_'+str(i)+'.npy', img_train_combined)
+    np.save(folder_name + "/train/spotlight/train_data_" + str(i) + ".npy", img_train_combined)
 
-print('Training data saved in '+folder_name+'/train/spotlight/')
+print("Training data saved in " + folder_name + "/train/spotlight/")
 
-#validation data
-size=256
+# validation data
+size = 256
 for i in range(sar.shape[-1]):
-    for j in range(l//256):
-        for k in range(L//256):
-            img_val_combined = np.zeros((256,256,2+nb_dim_sup))
-            img_val_combined[:,:,0] = np.real(sar[:,:,i])[k*size:(k+1)*size,j*size:(j+1)*size]
-            img_val_combined[:,:,1] = np.imag(sar[:,:,i])[k*size:(k+1)*size,j*size:(j+1)*size]
-            if args.method == 'SAR+OPT':
-                img_val_combined[:,:,2] = opt[k*size:(k+1)*size,j*size:(j+1)*size]
-            elif args.method == 'SAR+SAR':
-                if args.dataset == 'Saclay':
-                    img_val_combined[:,:,2] = np.abs(sar[:,:,3][k*size:(k+1)*size,j*size:(j+1)*size]) #higher resolution SAR image
-                    if i >=2: # in this case we only use the first three sar images
+    for j in range(l // 256):
+        for k in range(L // 256):
+            img_val_combined = np.zeros((256, 256, 2 + nb_dim_sup))
+            img_val_combined[:, :, 0] = np.real(sar[:, :, i])[
+                k * size : (k + 1) * size, j * size : (j + 1) * size
+            ]
+            img_val_combined[:, :, 1] = np.imag(sar[:, :, i])[
+                k * size : (k + 1) * size, j * size : (j + 1) * size
+            ]
+            if args.method == "SAR+OPT":
+                img_val_combined[:, :, 2] = opt[
+                    k * size : (k + 1) * size, j * size : (j + 1) * size
+                ]
+            elif args.method == "SAR+SAR":
+                if args.dataset == "Saclay":
+                    img_val_combined[:, :, 2] = np.abs(
+                        sar[:, :, 3][k * size : (k + 1) * size, j * size : (j + 1) * size]
+                    )  # higher resolution SAR image
+                    if i >= 2:  # in this case we only use the first three sar images
                         break
                 else:
-                    img_val_combined[:,:,2] = np.abs(sar[:,:,1][k*size:(k+1)*size,j*size:(j+1)*size])
-                    break # only two sar images in Sendai
-            elif args.method == 'SAR+OPT+SAR':
-                img_val_combined[:,:,2] = opt[k*size:(k+1)*size,j*size:(j+1)*size]
-                if args.dataset == 'Saclay':
-                    img_val_combined[:,:,3] = np.abs(sar[:,:,3][k*size:(k+1)*size,j*size:(j+1)*size]) 
-                    if i >=2:
+                    img_val_combined[:, :, 2] = np.abs(
+                        sar[:, :, 1][k * size : (k + 1) * size, j * size : (j + 1) * size]
+                    )
+                    break  # only two sar images in Sendai
+            elif args.method == "SAR+OPT+SAR":
+                img_val_combined[:, :, 2] = opt[
+                    k * size : (k + 1) * size, j * size : (j + 1) * size
+                ]
+                if args.dataset == "Saclay":
+                    img_val_combined[:, :, 3] = np.abs(
+                        sar[:, :, 3][k * size : (k + 1) * size, j * size : (j + 1) * size]
+                    )
+                    if i >= 2:
                         break
                 else:
-                    img_val_combined[:,:,3] = np.abs(sar[:,:,1][k*size:(k+1)*size,j*size:(j+1)*size])
+                    img_val_combined[:, :, 3] = np.abs(
+                        sar[:, :, 1][k * size : (k + 1) * size, j * size : (j + 1) * size]
+                    )
                     break
-            np.save(folder_name+'/validation/spotlight/val_data_'+str(i)+'_'+str(j)+'_'+str(k)+'.npy', img_val_combined)
+            np.save(
+                folder_name
+                + "/validation/spotlight/val_data_"
+                + str(i)
+                + "_"
+                + str(j)
+                + "_"
+                + str(k)
+                + ".npy",
+                img_val_combined,
+            )
 
-print('Validation data saved in '+folder_name+'/validation/spotlight/')
+print("Validation data saved in " + folder_name + "/validation/spotlight/")
--- src/vistools.py
+++ src/vistools.py
@@ -1,68 +1,122 @@
 import numpy as np
 import PIL.Image
-from  IPython.display import HTML, display
+from IPython.display import HTML, display
 from urllib import parse
 from io import BytesIO
 import base64
 
 
+def simple_equalization_8bit(im, percentiles=5):
+    """
+    Simple 8-bit requantization by linear stretching.
+
+    Args:
+        im (np.array): image to requantize
+        percentiles (int): percentage of the darkest and brightest pixels to saturate
+
+    Returns:
+        numpy array with the quantized uint8 image
+    """
+    mi, ma = np.percentile(im[np.isfinite(im)], (percentiles, 100 - percentiles))
+    im = np.clip(im, mi, ma)
+    im = (im - mi) / (ma - mi) * 255
+    return im.astype(np.uint8)
+
+
+def register(im1, im2, di, dj):
+    """
+    Register images with known displacement.
+
+    Parameters
+    ----------
+    im1, im2: ndarrays of same shape
+        images to register
+
+    di, dj: ints
+        displacement along the i- and j- directions to apply to the 2nd image to register it on the first
+    """
+    assert im1.shape == im2.shape
+    m, n = im1.shape
+    # make di and dj as small as possible in absolute value, using the periodicity of the translation
+    if di > m // 2:
+        di = di - m
+    if dj > n // 2:
+        dj = dj - n
+    # compute translated and cropped images
+    if di > 0:  # im2 must be translated to the right, i.e. im1 to the left
+        im1 = im1[di:]
+        im2 = im2[:-di]
+    elif di < 0:  # im2 must be translated to the left
+        im1 = im1[:di]
+        im2 = im2[-di:]
+    if dj > 0:  # im2 must be translated downwards, i.e. im1 upwards
+        im1 = im1[:, dj:]
+        im2 = im2[:, :-dj]
+    elif dj < 0:  # im2 must be translated upwards
+        im1 = im1[:, :dj]
+        im2 = im2[:, -dj:]
+    # return result
+    return im1, im2
+
+
 gallery_style_base = """
     <style>
-.gallery2 {
-    position: relative;
-    width: auto;
-    height: 650px; }
-.gallery2 .index {
-    padding: 0;
-    margin: 0;
-    width: 10.5em;
-    list-style: none; }
-.gallery2 .index li {
-    margin: 0;
-    padding: 0;
-    float: left;}
-.gallery2 .index a { /* gallery2 item title */
-    display: block;
-    background-color: #EEEEEE;
-    border: 1px solid #FFFFFF;
-    text-decoration: none;
-    width: 1.9em;
-    padding: 6px; }
-.gallery2 .index a span { /* gallery2 item content */
-    display: block;
-    position: absolute;
-    left: -9999px; /* hidden */
-    top: 0em;
-    padding-left: 0em; }
-.gallery2 .index a span img{ /* gallery2 item content */
-    height: 550px;
-    }
-.gallery2 .index li:first-child a span {
-    top: 0em;
-    left: 10.5em;
-    z-index: 99; }
-.gallery2 .index a:hover {
-    border: 1px solid #888888; }
-.gallery2 .index a:hover span {
-    left: 10.5em;
-    z-index: 100; }
-</style>
-    """
+        .gallery2 {
+            position: relative;
+            width: auto;
+            height: 650px; }
+        .gallery2 .index {
+            padding: 0;
+            margin: 0;
+            width: 10.5em;
+            list-style: none; }
+        .gallery2 .index li {
+            margin: 0;
+            padding: 0;
+            float: left;}
+        .gallery2 .index a { /* gallery2 item title */
+            display: block;
+            background-color: #EEEEEE;
+            border: 1px solid #FFFFFF;
+            text-decoration: none;
+            width: 1.9em;
+            padding: 6px; }
+        .gallery2 .index a span { /* gallery2 item content */
+            display: block;
+            position: absolute;
+            left: -9999px; /* hidden */
+            top: 0em;
+            padding-left: 0em; }
+        .gallery2 .index a span img{ /* gallery2 item content */
+            height: 550px;
+            }
+        .gallery2 .index li:first-child a span {
+            top: 0em;
+            left: 10.5em;
+            z-index: 99; }
+        .gallery2 .index a:hover {
+            border: 1px solid #888888; }
+        .gallery2 .index a:hover span {
+            left: 10.5em;
+            z-index: 100; }
+    </style>
+"""
 
 svg_overlay_style = """
-<style>
-.svg-overlay {
-  position: relative;
-  display: inline-block;
-}
+    <style>
+        .svg-overlay {
+            position: relative;
+            display: inline-block;
+        }
 
-.svg-overlay svg {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-</style>
+        .svg-overlay svg {
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
+    </style>
 """
+
 
 def urlencoded_jpeg_img(a):
     """
@@ -70,27 +124,27 @@ def urlencoded_jpeg_img(a):
     supports monochrome (shape = (N,M,1) or (N,M))
     and color arrays (N,M,3)
     """
-    fmt='jpeg'
-        
-    #handle color images (3,N,M) -> (N,M,3)
+    fmt = "jpeg"
+
+    # handle color images (3,N,M) -> (N,M,3)
     a = a.squeeze()
     if len(a.shape) == 3 and a.shape[0] == 3:
-        a = a.transpose(1,2,0)
+        a = a.transpose(1, 2, 0)
 
     f = BytesIO()
-    PIL.Image.fromarray(np.uint8(a).squeeze() ).save(f, fmt)
-    x =  base64.b64encode(f.getvalue())
-    return '''<img src="data:image/jpeg;base64,{}&#10;"/>'''.format(x.decode())
+    PIL.Image.fromarray(np.uint8(a).squeeze()).save(f, fmt)
+    x = base64.b64encode(f.getvalue())
+    return """<img src="data:image/jpeg;base64,{}&#10;"/>""".format(x.decode())
     # display using IPython.display.HTML(retval)
 
 
 def display_gallery(image_urls, image_labels=None, svg_overlays=None):
     """
-    image_urls can be a list of urls 
+    image_urls can be a list of urls
     or a list of numpy arrays
     image_labels is a list of strings
     """
-    
+
     gallery_template = """
     <div class="gallery2">
         <ul class="index">
@@ -98,7 +152,7 @@ def display_gallery(image_urls, image_labels=None, svg_overlays=None):
         </ul>
     </div>
     """
-    
+
     li_template = """<li><a href="#">{}<span style="background-color: white;  " ><img src="{}" /></br>{}</span></a></li>"""
     li_template_encoded = """<li><a href="#">{}<span style="background-color: white;  " >{}</br>{}</span></a></li>"""
 
@@ -115,20 +169,20 @@ def display_gallery(image_urls, image_labels=None, svg_overlays=None):
         else:
             svg = None
 
-        if type(u) == str and parse.urlparse(u).scheme in ('http', 'https', 'ftp'):  # full url
-            li = li + li_template.format( idx, u, label)
-        elif type(u) == str:   # assume relative url path
+        if type(u) == str and parse.urlparse(u).scheme in ("http", "https", "ftp"):  # full url
+            li = li + li_template.format(idx, u, label)
+        elif type(u) == str:  # assume relative url path
             img = np.asarray(PIL.Image.open(u))
-            li = li + li_template_encoded.format( idx, urlencoded_jpeg_img(img), label)                
-        elif type(u) == np.ndarray:   # input is np.array
+            li = li + li_template_encoded.format(idx, urlencoded_jpeg_img(img), label)
+        elif type(u) == np.ndarray:  # input is np.array
             h, w = u.shape[0], u.shape[1]
             div = f'<div class="svg-overlay">{urlencoded_jpeg_img(u)}<svg viewBox="0 0 {w} {h}">{svg}</svg></div>'
             li = li + li_template_encoded.format(idx, div, label)
 
         idx = idx + 1
-        
+
     source = gallery_template.format(li)
-    
+
     display(HTML(source))
     display(HTML(gallery_style_base))
     display(HTML(svg_overlay_style))
